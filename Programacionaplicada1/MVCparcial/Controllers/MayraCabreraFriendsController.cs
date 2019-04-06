13a11286fa42c0d160f715182804125a27acee49
Mayra Cabrera <mayracabrerajaldin@gmail.com>
2019-04-06 10:04:44 -0400

    Commit MVC con modelo

---
+++ Programacionaplicada1/MVCparcial/Controllers/MayraCabreraFriendsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCparcial.Models;
+
+namespace MVCparcial.Controllers
+{
+    public class MayraCabreraFriendsController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        // GET: MayraCabreraFriends
+        public ActionResult Index()
+        {
+            return View(db.MayraCabreraFriends.ToList());
+        }
+
+        // GET: MayraCabreraFriends/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MayraCabreraFriend mayraCabreraFriend = db.MayraCabreraFriends.Find(id);
+            if (mayraCabreraFriend == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mayraCabreraFriend);
+        }
+
+        // GET: MayraCabreraFriends/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MayraCabreraFriends/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FriendId,Name,DateType,TipoAmigo")] MayraCabreraFriend mayraCabreraFriend)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MayraCabreraFriends.Add(mayraCabreraFriend);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(mayraCabreraFriend);
+        }
+
+        // GET: MayraCabreraFriends/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MayraCabreraFriend mayraCabreraFriend = db.MayraCabreraFriends.Find(id);
+            if (mayraCabreraFriend == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mayraCabreraFriend);
+        }
+
+        // POST: MayraCabreraFriends/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "FriendId,Name,DateType,TipoAmigo")] MayraCabreraFriend mayraCabreraFriend)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(mayraCabreraFriend).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(mayraCabreraFriend);
+        }
+
+        // GET: MayraCabreraFriends/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MayraCabreraFriend mayraCabreraFriend = db.MayraCabreraFriends.Find(id);
+            if (mayraCabreraFriend == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mayraCabreraFriend);
+        }
+
+        // POST: MayraCabreraFriends/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MayraCabreraFriend mayraCabreraFriend = db.MayraCabreraFriends.Find(id);
+            db.MayraCabreraFriends.Remove(mayraCabreraFriend);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Programacionaplicada1/MVCparcial/Models/MayraCabreraFriend.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCparcial.Models
+{
+    public enum FriendType
+    {
+        Conocido,
+        CompañeroEstudio,
+        ColegaTrbajo,
+        Amigo,
+        AmigoInfancia,
+        Pariente
+
+    }
+
+
+
+    public class MayraCabreraFriend
+    {
+
+
+        [Key]
+        public int FriendId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string DateType { get; set; }
+        public FriendType TipoAmigo { get; set; }
+        public string Apodo { get; set; }
+        
+
+    }
+}
